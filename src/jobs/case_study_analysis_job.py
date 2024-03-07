da6504981fa76848e644410a78470260098b6f5d
Brillianttyagi <deepanshubhai84@gmail.com>
2024-03-07 11:04:18 +0530

    Added print and save csv and run functionality.

--- src/jobs/case_study_analysis_job.py
+++ src/jobs/case_study_analysis_job.py
@@ -1,9 +1,14 @@
+"""
+This module contains the CaseStudyAnalysis class, 
+which performs various analysis on case study data.
+"""
+
 import pyspark.sql.functions as F
-from pyspark.sql import DataFrame
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 from pyspark.sql.window import Window
 
 from src.constants import constants
-from src.utils.file_utils import read_csv
+from src.utils.file_utils import read_csv, write_csv
 
 
 class CaseStudyAnalysis:
@@ -42,12 +47,18 @@ class CaseStudyAnalysis:
 
     def __init__(self, spark, config):
         files_path = config.get("INPUT_FILESNAME")
-        self.changes_use = read_csv(spark, files_path.get("CHANGES_USE"))
-        self.damages_use = read_csv(spark, files_path.get("DAMAGES_USE"))
-        self.endorse_use = read_csv(spark, files_path.get("ENDORSE_USE"))
-        self.primary_person_use = read_csv(spark, files_path.get("PRIMARY_PERSON_USE"))
-        self.restrict_use = read_csv(spark, files_path.get("RESTRICT_USE"))
-        self.units_use = read_csv(spark, files_path.get("UNITS_USE"))
+        file_format = config.get("FILE_FORMATS").get("INPUT")
+        self.charges_use = read_csv(spark, files_path.get("CHANGES_USE"), file_format)
+        self.damages_use = read_csv(spark, files_path.get("DAMAGES_USE"), file_format)
+        self.endorse_use = read_csv(spark, files_path.get("ENDORSE_USE"), file_format)
+        self.primary_person_use = read_csv(
+            spark, files_path.get("PRIMARY_PERSON_USE"), file_format
+        )
+        self.restrict_use = read_csv(spark, files_path.get("RESTRICT_USE"), file_format)
+        self.units_use = read_csv(spark, files_path.get("UNITS_USE"), file_format)
+        self.spark_session = spark
+        self.output_path = config.get("OUTPUT_PATH").get("CASE_STUDY_JOB")
+        self.output_format = config.get("FILE_FORMATS").get("OUTPUT")
 
     def count_crashes_where_males_killed(self) -> int:
         """
@@ -80,7 +91,7 @@ class CaseStudyAnalysis:
             F.col(constants.VEH_BODY_STYL_ID).contains("MOTORCYCLE")
         ).count()
 
-    def analyse_fatal_crashes(self) -> DataFrame:
+    def analyse_fatal_crashes(self) -> list:
         """
         Analyse the top 5 vehicle makes of the cars present in the crashes in
         which the driver died and airbags did not deploy.
@@ -103,7 +114,7 @@ class CaseStudyAnalysis:
         )
 
         # Perform the join and analysis
-        return (
+        veh_df = (
             units_use_filtered.join(
                 primary_person_use_filtered, constants.CRASH_ID, "inner"
             )
@@ -113,6 +124,8 @@ class CaseStudyAnalysis:
             .limit(5)
             .select(constants.VEH_MAKE_ID)
         )
+
+        return [row[0] for row in veh_df.collect()]
 
     def count_vehicles_with_special_conditions(self) -> int:
         """
@@ -144,7 +157,7 @@ class CaseStudyAnalysis:
             units_use_filtered, constants.CRASH_ID, "inner"
         ).count()
 
-    def count_drivers_by_license_state(self) -> DataFrame:
+    def drivers_by_license_state(self) -> list:
         """
         State has highest number of accidents in which females are not involved.
 
@@ -155,7 +168,7 @@ class CaseStudyAnalysis:
         DataFrame: First state name.
         """
         # Perform the analysis and return the result
-        return (
+        drvr_df = (
             self.primary_person_use.filter(F.col(constants.PRSN_GNDR_ID) != "FEMALE")
             .groupBy(constants.DRVR_LIC_STATE_ID)
             .count()
@@ -164,7 +177,9 @@ class CaseStudyAnalysis:
             .first()
         )
 
-    def analyse_injuries_by_vehicle_make(self) -> DataFrame:
+        return [row[0] for row in drvr_df.collect()]
+
+    def analyse_injuries_by_vehicle_make(self) -> list:
         """
         the Top 3rd to 5th VEH_MAKE_IDs that contribute to a
         largest number of injuries including death.
@@ -193,9 +208,11 @@ class CaseStudyAnalysis:
         )
 
         # Return the result DataFrame with the top 3 to 5 vehicle makes
-        return result_df.limit(5).subtract(result_df.limit(2))
+        veh_df = result_df.limit(5).subtract(result_df.limit(2))
 
-    def clean_and_aggregate_data(self) -> DataFrame:
+        return [row[0] for row in veh_df.collect()]
+
+    def clean_and_aggregate_data(self) -> list:
         """
         All the body styles involved in crashes, mention the top
         ethnic user group of each unique body style
@@ -231,7 +248,7 @@ class CaseStudyAnalysis:
         )
 
         # Join and aggregate data
-        return (
+        veh_df = (
             primary_person_use_filtered.join(
                 units_use_filtered, constants.CRASH_ID, "inner"
             )
@@ -242,7 +259,9 @@ class CaseStudyAnalysis:
             .drop("row", "count")
         )
 
-    def analyse_alcohol_contributions(self) -> DataFrame:
+        return [row[0] for row in veh_df.collect()]
+
+    def analyse_alcohol_contributions(self) -> list:
         """
         The Top 5 Zip Codes with highest number crashes with
         alcohols as the contributing factor to a crash.
@@ -255,7 +274,7 @@ class CaseStudyAnalysis:
                    alcohol-related contributions.
         """
         # Perform the analysis and return the result
-        return (
+        drvr_df = (
             self.primary_person_use.dropna(subset=[constants.DRVR_ZIP])
             .join(self.units_use, constants.CRASH_ID, "inner")
             .filter(
@@ -265,10 +284,13 @@ class CaseStudyAnalysis:
             .groupBy(constants.DRVR_ZIP)
             .agg(F.count("*").alias("count"))
             .orderBy(F.col("count").desc())
+            .select(constants.DRVR_ZIP)
             .limit(5)
         )
 
-    def filter_units_and_damages(self) -> DataFrame:
+        return [row[0] for row in drvr_df.collect()]
+
+    def filter_units_and_damages(self) -> list:
         """
         Count of Distinct Crash IDs where No Damaged Property was observed
         and Damage Level (VEH_DMAG_SCL~) is above 4 and car avails Insurance.
@@ -326,71 +348,140 @@ class CaseStudyAnalysis:
         )
 
         # Perform the join and return the result
-        return units_use_filtered.join(
+        crsh_df = units_use_filtered.join(
             damage_use_filtered, constants.CRASH_ID, "inner"
         ).select(constants.CRASH_ID)
 
+        return [row[0] for row in crsh_df.collect()]
 
-def analyse_units_charges(self) -> DataFrame:
-    """
-    The Top 5 Vehicle Makes where drivers are charged with
-    speeding related offences, has licensed Drivers, used
-    top 10 used vehicle colours and has car licensed with
-    the Top 25 states with highest number
-    of offences.
+    def analyse_units_charges(self) -> list:
+        """
+        The Top 5 Vehicle Makes where drivers are charged with
+        speeding related offences, has licensed Drivers, used
+        top 10 used vehicle colours and has car licensed with
+        the Top 25 states with highest number
+        of offences.
 
-    Parameters:
-    self: An instance of the class.
+        Parameters:
+        self: An instance of the class.
 
-    Returns:
-    DataFrame: The result of the analysis, showing the top 5 vehicle
-               makes with charges related to speeding.
-    """
-    # Find the top 10 vehicle colors
-    top_10_vehicle_color = (
-        self.units_use.filter(F.col(constants.VEH_COLOR_ID) != "NA")
-        .groupBy(constants.VEH_COLOR_ID)
-        .count()
-        .orderBy(F.col("count").desc())
-        .select(constants.VEH_COLOR_ID)
-        .limit(10)
-        .collect()
-    )
-    top_10_vehicle_color_list = [row[0] for row in top_10_vehicle_color]
-
-    # Find the top 25 states with the highest number of offenses
-    # filter is done because VEH_LIC_STATE_ID contains some integer values
-    top_25_states_highest_nmbr_offences = (
-        self.units_use.filter(F.col(constants.VEH_LIC_STATE_ID).cast("int").isNull())
-        .groupBy(constants.VEH_LIC_STATE_ID)
-        .count()
-        .orderBy(F.col(constants.VEH_LIC_STATE_ID).desc())
-        .limit(25)
-        .collect()
-    )
-    top_25_states_highest_nmbr_offences_list = [
-        row[0] for row in top_25_states_highest_nmbr_offences
-    ]
-
-    # Join units_use, charges_use, and primary_person_use DataFrames
-    return (
-        self.units_use.join(self.charges_use, constants.CRASH_ID, "inner")
-        .join(self.primary_person_use, constants.CRASH_ID, "inner")
-        .filter(F.col(constants.CHARGE).contains("SPEED"))
-        .filter(
-            F.col(constants.DRVR_LIC_TYPE_ID).isin(
-                "COMMERCIAL DRIVER LIC.", "DRIVER LICENSE"
-            )
+        Returns:
+        DataFrame: The result of the analysis, showing the top 5 vehicle
+                makes with charges related to speeding.
+        """
+        # Find the top 10 vehicle colors
+        top_10_vehicle_color = (
+            self.units_use.filter(F.col(constants.VEH_COLOR_ID) != "NA")
+            .groupBy(constants.VEH_COLOR_ID)
+            .count()
+            .orderBy(F.col("count").desc())
+            .select(constants.VEH_COLOR_ID)
+            .limit(10)
+            .collect()
         )
-        .filter(
-            F.col(constants.VEH_COLOR_ID).isin(top_10_vehicle_color_list)
-            & F.col(constants.VEH_LIC_STATE_ID).isin(
-                top_25_states_highest_nmbr_offences_list
+        top_10_vehicle_color_list = [row[0] for row in top_10_vehicle_color]
+
+        # Find the top 25 states with the highest number of offenses
+        # filter is done because VEH_LIC_STATE_ID contains some integer values
+        top_25_states_highest_nmbr_offences = (
+            self.units_use.filter(
+                F.col(constants.VEH_LIC_STATE_ID).cast("int").isNull()
             )
+            .groupBy(constants.VEH_LIC_STATE_ID)
+            .count()
+            .orderBy(F.col(constants.VEH_LIC_STATE_ID).desc())
+            .limit(25)
+            .collect()
         )
-        .groupBy(constants.VEH_MAKE_ID)
-        .count()
-        .orderBy(F.col("count").desc())
-        .select(constants.VEH_MAKE_ID)
-        .limit(5)
-    )
+        top_25_states_highest_nmbr_offences_list = [
+            row[0] for row in top_25_states_highest_nmbr_offences
+        ]
+
+        # Join units_use, charges_use, and primary_person_use DataFrames
+        veh_df = (
+            self.units_use.join(self.charges_use, constants.CRASH_ID, "inner")
+            .join(self.primary_person_use, constants.CRASH_ID, "inner")
+            .filter(F.col(constants.CHARGE).contains("SPEED"))
+            .filter(
+                F.col(constants.DRVR_LIC_TYPE_ID).isin(
+                    "COMMERCIAL DRIVER LIC.", "DRIVER LICENSE"
+                )
+            )
+            .filter(
+                F.col(constants.VEH_COLOR_ID).isin(top_10_vehicle_color_list)
+                & F.col(constants.VEH_LIC_STATE_ID).isin(
+                    top_25_states_highest_nmbr_offences_list
+                )
+            )
+            .groupBy(constants.VEH_MAKE_ID)
+            .count()
+            .orderBy(F.col("count").desc())
+            .select(constants.VEH_MAKE_ID)
+            .limit(5)
+        )
+
+        return [row[0] for row in veh_df.collect()]
+
+    def save_format_csv(self, analysis_set) -> None:
+        """
+        This function formats the output of the analysis into a DataFrame
+
+        Parameters:
+        self: An instance of the class.
+        analysis_set: The result of the analysis
+
+        Returns:
+        None
+        """
+        # Define the schema for the DataFrame
+        schema = StructType(
+            [
+                StructField("analysis_no", IntegerType(), True),
+                StructField("result", StringType(), True),
+            ]
+        )
+
+        # Create a DataFrame from the results
+        result_df = self.spark_session.createDataFrame(analysis_set, schema=schema)
+
+        # Save the DataFrame to CSV
+        write_csv(result_df, self.output_path, self.output_format)
+
+    def run(self):
+        """
+        Run the case study analysis job.
+
+        Parameters:
+        self: An instance of the class.
+
+        Returns:
+        None
+        """
+        analysis_1_output = self.count_crashes_where_males_killed()
+        analysis_2_output = self.count_two_wheelers_crashes()
+        analysis_3_output = ",".join(self.analyse_fatal_crashes())
+        analysis_4_output = self.count_vehicles_with_special_conditions()
+        analysis_5_output = ",".join(self.drivers_by_license_state())
+        analysis_6_output = ",".join(self.analyse_injuries_by_vehicle_make())
+        analysis_7_output = ",".join(self.clean_and_aggregate_data())
+        analysis_8_output = ",".join(self.analyse_alcohol_contributions())
+        analysis_9_output = ",".join(self.filter_units_and_damages())
+        analysis_10_output = ",".join(self.analyse_units_charges())
+
+        analysis_set = (
+            ("Analysis 1", analysis_1_output),
+            ("Analysis 2", analysis_2_output),
+            ("Analysis 3", analysis_3_output),
+            ("Analysis 4", analysis_4_output),
+            ("Analysis 5", analysis_5_output),
+            ("Analysis 6", analysis_6_output),
+            ("Analysis 7", analysis_7_output),
+            ("Analysis 8", analysis_8_output),
+            ("Analysis 9", analysis_9_output),
+            ("Analysis 10", analysis_10_output),
+        )
+        # save the results to a file
+        self.save_format_csv(analysis_set)
+        # Print the results
+        for analysis in analysis_set:
+            print(f"{analysis[0]}: {analysis[1]}")
