1ad265492c1fab1fd10ff4489b188f852f72a492
Brillianttyagi <deepanshubhai84@gmail.com>
2024-03-07 11:32:10 +0530

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="BCG_Analysis",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["pyspark==2.4.5", "pyyaml==5.3.1"],
+    install_requires=["pyspark==3.5.1", "pyyaml==6.0.1"],
     author="Deepanshu Tyagi",
     description="This is a case study analysis project for BCG.",
     url="https://github.com/Brillianttyagi/Case_Study.git",
