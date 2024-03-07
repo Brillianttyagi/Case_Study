e03a0143b764bc6ffb0ce48f1a9c291923e9730b
Brillianttyagi <deepanshubhai84@gmail.com>
2024-03-07 14:55:35 +0530

    Refactor file reading and writing functions with error handling

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -16,8 +16,11 @@ def read_yaml(path: str) -> dict:
     Returns:
         A dictionary containing the data from the YAML file.
     """
-    with open(path, "r", encoding="utf-8") as file:
-        return yaml.safe_load(file)
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            return yaml.safe_load(file)
+    except Exception as err:
+        raise ValueError(f"Error reading YAML file: {err}") from err
 
 
 def read_csv(spark: SparkSession, path: str, fileformat: str) -> DataFrame:
@@ -32,12 +35,15 @@ def read_csv(spark: SparkSession, path: str, fileformat: str) -> DataFrame:
     Returns:
         A DataFrame containing the data from the CSV file.
     """
-    return (
-        spark.read.format(fileformat)
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .load(path)
-    )
+    try:
+        return (
+            spark.read.format(fileformat)
+            .option("header", "true")
+            .option("inferSchema", "true")
+            .load(path)
+        )
+    except Exception as err:
+        raise ValueError(f"Error reading CSV file: {err}") from err
 
 
 def write_csv(df: DataFrame, path: str, fileformat: str) -> None:
@@ -49,6 +55,9 @@ def write_csv(df: DataFrame, path: str, fileformat: str) -> None:
         path: The path to the output CSV file.
         fileformat: The format of the file, e.g., "csv", "parquet", etc.
     """
-    df.repartition(1).write.format(fileformat).mode("overwrite").option(
-        "header", "true"
-    ).save(path)
+    try:
+        df.repartition(1).write.format(fileformat).mode("overwrite").option(
+            "header", "true"
+        ).save(path)
+    except Exception as err:
+        raise ValueError(f"Error writing CSV file: {err}") from err
